74cd7651939d058802bb5b9be0229437216b7037
Andrew Johnson <andrewjohnson@Andrews-MBP.home>
2017-05-10 15:54:35 -0400

    when lost in code, just start naming all the things you know

--- src/view.rs
+++ src/view.rs
@@ -1,18 +1,40 @@
 pub struct Square {
-   pub width: f64
+   width: f64,
+   dirty: bool
 }
 impl Square {
    pub fn new(w: f64) -> Square {
-      Square { width:w }
+      Square { width:w, dirty:true }
+   }
+   pub fn width(&self) -> f64 {
+      self.width
+   }
+   pub fn set_width(&mut self, w: f64) {
+      self.width = w;
+      self.dirty = true;
+   }
+   pub fn clean(&mut self) {
+      self.dirty = false;
    }
 }
 
 pub struct Circle {
-   pub radius: f64
+   radius: f64,
+   dirty: bool
 }
 impl Circle {
    pub fn new(r: f64) -> Circle {
-      Circle { radius:r }
+      Circle { radius:r, dirty:true }
+   }
+   pub fn radius(&self) -> f64 {
+      self.radius
+   }
+   pub fn set_radius(&mut self, r: f64) {
+      self.radius = r;
+      self.dirty = true;
+   }
+   pub fn clean(&mut self) {
+      self.dirty = false;
    }
 }
 
