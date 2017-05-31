fcb51fa6eeff7e815a5edf67dea76fb02d5aa1cc
Andrew Johnson <andrewjohnson@Andrews-MBP.home>
2017-05-31 07:10:41 -0400

    shadow interface

--- examples/events.rs
+++ examples/events.rs
@@ -10,14 +10,14 @@ fn main() {
     w.start(|events| {
        let mut v = View::new();
        v.append(Text::new("assets/Macondo-Regular.ttf", "hover text")
-               .hover(|e, i| { i.clone().shadow(-3, -3, 3, 3) })
+               .hover(|e, i| { i.clone().shadow(-3, -3, 3, 3, [0.4, 0.4, 0.4, 1.0]) })
                .color([0.4, 0.4, 1.0, 1.0])
                .scale(2.0, "em")
                .width(25.0, "%")
                .translate_x(150.0, "px")
                .translate_y(150.0, "px"));
        v.append(Text::new("assets/Macondo-Regular.ttf", "click text")
-               .click(|e, i| { i.shadow(-3, -3, 3, 3) })
+               .click(|e, i| { i.shadow(-3, -3, 3, 3, [0.4, 0.4, 0.4, 1.0]) })
                .color([1.0, 0.4, 0.4, 1.0])
                .scale(3.0, "em")
                .width(40.0, "%")
