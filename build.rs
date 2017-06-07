044799f750d9a44a36346919894a6b81b47f6a53
Andrew Johnson <andrewjohnson@Andrews-MBP.home>
2017-06-07 15:25:37 -0400

    dynamically generate example assets with cfg! platform switch

--- build.rs
+++ build.rs
@@ -8,9 +8,16 @@ pub fn main() {
       "assets/handcloth.png",
       "assets/Macondo-Regular.ttf"];
    let mut f = File::create(Path::new("examples").join("assets.in")).unwrap();
+   f.write_all(b"[");
    if cfg!(windows) {
+      for ast in example_assets.iter() {
+         f.write_all( format!("(\"{}\",include_bytes!(\"{}\").to_vec()),",ast,ast.to_string().replace("/","\\")).as_bytes() );
+      }
       f.write_all(b"Hello Windows");
    } else {
-      f.write_all(b"Hello Other");
+      for ast in example_assets.iter() {
+         f.write_all( format!("(\"{}\",include_bytes!(\"{}\").to_vec()),",ast,ast.to_string()).as_bytes() );
+      }
    }
+   f.write_all(b"]");
 }
